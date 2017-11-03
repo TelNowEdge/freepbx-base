b8ead94c836a21b0119fcfad9809a6df68e27aa4
Boris Morel <boris.morel@telnowedge.com>
2017-11-03 15:46:12 +0100

    [ADD] Migration process

---
+++ Resources/Migrations/AbstractMigration.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace TelNowEdge\FreePBX\Base\Resources\Migrations;
+
+abstract class AbstractMigration implements MigrationInterface
+{
+    /**
+     * \FreePBX\Database.
+     */
+    protected $connection;
+
+    public function setConnection(\FreePBX\Database $database)
+    {
+        $this->connection = $database->getDoctrineConnection();
+        $this->connection->setFetchMode(\PDO::FETCH_OBJ);
+    }
+
+    public function migrate()
+    {
+        $sql = array();
+        $reflector = new \ReflectionClass(static::class);
+        $methods = $reflector->getMethods();
+
+        foreach ($methods as $method) {
+            if (1 !== preg_match('/^migration\d{8}/', $method->name)) {
+                continue;
+            }
+
+            try {
+                $this->connection->executeUpdate($method->invoke($this));
+            } catch (\Exception $e) {
+
+            }
+        }
+    }
+}
---
+++ Resources/Migrations/MigrationInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TelNowEdge\FreePBX\Base\Resources\Migrations;
+
+interface MigrationInterface
+{
+    public function setConnection(\FreePBX\Database $connection);
+    public function migrate();
+}
