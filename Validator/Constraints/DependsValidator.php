71e7cc7f5213ed014ce93ca833ad450adbed016a
Boris Morel <boris.morel@telnowedge.com>
2018-01-03 14:23:06 +0100

    [FIX] Typo

--- Validator/Constraints/DependsValidator.php
+++ Validator/Constraints/DependsValidator.php
@@ -16,47 +16,47 @@ class DependsValidator extends ConstraintValidator implements ContainerAwareInte
         $this->container = $container;
     }
 
-    public function validate($obj, Constraint $consraint)
+    public function validate($obj, Constraint $constraint)
     {
-        if (false === $this->container->has($consraint->service[0])) {
+        if (false === $this->container->has($constraint->service[0])) {
             $this->context
                 ->buildViolation('Unable to find service: {{ service }}')
-                ->setParameter('{{ service }}', $consraint->service[0])
+                ->setParameter('{{ service }}', $constraint->service[0])
                 ->addViolation()
                 ;
         }
 
-        $service = $this->container->get($consraint->service[0]);
+        $service = $this->container->get($constraint->service[0]);
 
         $reflector = new \ReflectionClass($service);
 
-        if (false === $reflector->hasMethod($consraint->service[1])) {
+        if (false === $reflector->hasMethod($constraint->service[1])) {
             $this->context
                 ->buildViolation('Unable to find method: {{ method }}')
-                ->setParameter('{{ method }}', $consraint->service[1])
+                ->setParameter('{{ method }}', $constraint->service[1])
                 ->addViolation()
                 ;
         }
 
         $reflModel = new \ReflectionClass($obj);
-        if (false === $reflModel->hasMethod(sprintf('get%s', ucfirst($consraint->field)))
-        || false === $reflModel->hasMethod(sprintf('get%s', ucfirst($consraint->depends)))
+        if (false === $reflModel->hasMethod(sprintf('get%s', ucfirst($constraint->field)))
+        || false === $reflModel->hasMethod(sprintf('get%s', ucfirst($constraint->depends)))
         ) {
             $this->context
                 ->buildViolation('Unable to find a least one of this methods: {{ methods }}')
-                ->setParameter('{{ methods }}', sprintf('%s or %s', $consraint->field, $consraint->depends))
+                ->setParameter('{{ methods }}', sprintf('%s or %s', $constraint->field, $constraint->depends))
                 ->addViolation()
                 ;
         }
 
-        $method = $reflector->getMethod($consraint->service[1]);
-        $dependsMethod = $reflModel->getMethod(sprintf('get%s', ucfirst($consraint->depends)));
-        $fieldMethod = $reflModel->getMethod(sprintf('get%s', ucfirst($consraint->field)));
+        $method = $reflector->getMethod($constraint->service[1]);
+        $dependsMethod = $reflModel->getMethod(sprintf('get%s', ucfirst($constraint->depends)));
+        $fieldMethod = $reflModel->getMethod(sprintf('get%s', ucfirst($constraint->field)));
 
         if (false === in_array($fieldMethod->invoke($obj), $method->invoke($service, $dependsMethod->invoke($obj)), true)) {
             $this->context
-                ->buildViolation($consraint->message)
-                ->atPath($consraint->field)
+                ->buildViolation($constraint->message)
+                ->atPath($constraint->field)
                 ->addViolation()
                 ;
         }
