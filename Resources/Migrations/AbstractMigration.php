2117a3650110b5ed42efd8ad56728babe4c3e4a2
Boris Morel <boris.morel@telnowedge.com>
2018-01-23 13:57:22 +0100

    [ADD] Lite migration control

--- Resources/Migrations/AbstractMigration.php
+++ Resources/Migrations/AbstractMigration.php
@@ -2,6 +2,8 @@
 
 namespace TelNowEdge\FreePBX\Base\Resources\Migrations;
 
+use Doctrine\DBAL\Exception\TableNotFoundException;
+
 abstract class AbstractMigration implements MigrationInterface
 {
     /**
@@ -17,20 +19,71 @@ abstract class AbstractMigration implements MigrationInterface
 
     public function migrate()
     {
-        $sql = array();
+        $this->checkDb();
+
+        $error = false;
         $reflector = new \ReflectionClass(static::class);
         $methods = $reflector->getMethods();
 
+        $this->connection->beginTransaction();
+
         asort($methods);
         foreach ($methods as $method) {
-            if (1 !== preg_match('/^migration\d{10}$/', $method->name)) {
+            if (1 !== preg_match('/^migration(\d{10})$/', $method->name, $match)) {
+                continue;
+            }
+
+            if (true === $this->alreadyMigrate($match[1], static::class)) {
                 continue;
             }
 
             try {
                 $this->connection->executeUpdate($method->invoke($this));
+                $this->connection->executeQuery(
+                    'INSERT INTO `tne_migrations` VALUES (?, ?, NOW())',
+                    array(
+                        $match[1],
+                        static::class,
+                    )
+                );
             } catch (\Exception $e) {
+                outn($e->getMessage());
+                $error = true;
             }
         }
+
+        if (true === $error) {
+            $this->connection->rollBack();
+
+            return false;
+        }
+
+        $this->connection->commit();
+
+        return true;
+    }
+
+    private function checkDb()
+    {
+        try {
+            $this->connection->executeQuery('desc tne_migrations');
+        } catch (TableNotFoundException $e) {
+            $this->connection->executeQuery('
+CREATE
+    TABLE
+        tne_migrations (
+            `id` INT PRIMARY KEY
+            ,`module` VARCHAR(255) NOT NULL
+            ,created_at DATETIME NOT NULL
+        )
+            ');
+        }
+    }
+
+    private function alreadyMigrate($version, $module)
+    {
+        $stmt = $this->connection->executeQuery('SELECT * FROM tne_migrations WHERE id = ? AND module = ?', array($version, $module));
+
+        return false === $stmt->fetch() ? false : true;
     }
 }
