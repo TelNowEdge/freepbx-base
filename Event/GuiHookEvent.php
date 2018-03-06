701f89986d462fa51b08a9604f384b16b2921fae
Boris Morel <boris.morel@telnowedge.com>
2018-03-06 09:54:05 +0100

    [ADD] Add event PRE_RENDERING

--- Event/GuiHookEvent.php
+++ Event/GuiHookEvent.php
@@ -8,6 +8,7 @@ class GuiHookEvent extends Event
 {
     const GUI_HOOK_START = 'gui_hook.start';
     const GUI_HOOK_DONE = 'gui_hook.done';
+    const GUI_HOOK_PRE_RENDERING = 'gui_hook.pre_rendering';
 
     protected $module;
 
