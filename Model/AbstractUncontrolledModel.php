0fc4fabdd6381c8aa70f2d8512aeedb88870cfe9
Boris Morel <boris.morel@telnowedge.com>
2018-03-19 16:29:38 +0100

    [ADD] UncotrollerModel

---
+++ Model/AbstractUncontrolledModel.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * Copyright [2016] [TelNowEdge]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace TelNowEdge\FreePBX\Base\Model;
+
+class AbstractUncontrolledModel
+{
+    private $storage;
+
+    public function __construct()
+    {
+        $this->storage = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->storage->set('keys', new \Doctrine\Common\Collections\ArrayCollection());
+    }
+
+    public function __call($name, $args)
+    {
+        $value = reset($args);
+
+        if (1 === preg_match('/^set(.*)$/', $name, $match)) {
+            $prop = lcfirst($match[1]);
+
+            if (1 === preg_match('/^key(\d{3})$/', $prop, $subMatch)) {
+                $this->storage->get('keys')->set($subMatch[1], $value);
+
+                return;
+            }
+
+            $this->storage->set(lcfirst($prop), $value);
+        }
+
+        if (1 === preg_match('/^get(.*)$/', $name, $match)) {
+            return $this->storage->get(lcfirst($match[1]));
+        }
+    }
+
+    public function getAll()
+    {
+        return $this->storage->toArray();
+    }
+
+    public function getKeys()
+    {
+        return $this->storage->get('keys');
+    }
+}
