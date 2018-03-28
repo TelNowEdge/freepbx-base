53626d7e8de1aac28d53ef04de85c0a8e806d298
Boris Morel <boris.morel@telnowedge.com>
2018-03-28 09:24:54 +0200

    [FIX] Prevent error with circular reference

---
+++ Serializer/Normalizer/ObjectNormalizerFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * Copyright [2016] [TelNowEdge]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace TelNowEdge\FreePBX\Base\Serializer\Normalizer;
+
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+class ObjectNormalizerFactory
+{
+    public function getObjectNormalizer()
+    {
+        $normalizer = new ObjectNormalizer();
+        $normalizer->setCircularReferenceLimit(2);
+        $normalizer->setCircularReferenceHandler(function ($object) {
+            return $object->getId();
+        });
+
+        return $normalizer;
+    }
+}
