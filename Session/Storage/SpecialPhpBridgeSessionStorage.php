dfe33b8820db5e4ab3e284caabc01594349d16a6
Boris Morel <boris.morel@telnowedge.com>
2019-06-05 14:30:48 +0200

    [FIX][MISSING] Include the file is better

---
+++ Session/Storage/SpecialPhpBridgeSessionStorage.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * Copyright 2019 TelNowEdge
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace TelNowEdge\FreePBX\Base\Session\Storage;
+
+use Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorage;
+
+class SpecialPhpBridgeSessionStorage extends NativeSessionStorage
+{
+    public function __construct($handler = null, MetadataBag $metaBag = null)
+    {
+        $this->setMetadataBag($metaBag);
+        $this->setSaveHandler($handler);
+    }
+
+    public function start()
+    {
+        if ($this->started) {
+            return true;
+        }
+
+        if (\PHP_SESSION_ACTIVE === session_status()) {
+            $this->loadSession();
+
+            return true;
+        }
+
+        parent::start();
+
+        return true;
+    }
+
+    public function clear()
+    {
+        // clear out the bags and nothing else that may be set
+        // since the purpose of this driver is to share a handler
+        foreach ($this->bags as $bag) {
+            $bag->clear();
+        }
+
+        // reconnect the bags to the session
+        $this->loadSession();
+    }
+}
