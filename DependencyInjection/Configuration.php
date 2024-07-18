9465fbb8d670a368dd64883bb482de14db3eb7df
Dorine Journet <dorine.journet@telnowedge.com>
2024-07-18 09:09:16 +0000

    [UPDATE] typehint

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -23,7 +23,7 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
 {
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         return new TreeBuilder('base_freepbx');
     }
